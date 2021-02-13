966be47e4ef0304ef92ac4445814a2d7848c4f85
bes01 <55254691+bes01@users.noreply.github.com>
2021-02-13 17:00:54 +0400

    test

--- Drinks_Project/src/js/navigation.js
+++ Drinks_Project/src/js/navigation.js
@@ -18,6 +18,7 @@ const markActive = (page) =>{
 }
 
 let goToPage = async (page, from) =>{
+    console.log(page + " a");
     /* From Ingredient View To Search Page Case */
     window.location.hash = page;
     /* * * * * * ** * * * * * * * * * * * * * * */
